85b6cc63ec15dc32fde5d98940b625abca9d5c65
Eunhak Lee <admin@return0927.xyz>
2019-07-11 19:34:59 +0900

    Fixed initializing error of status classes

--- Chem/status.py
+++ Chem/status.py
@@ -6,32 +6,22 @@
 
 class Status:
     status = ""
-    temperature = 273
+
+    def __init__(self, temp=273):
+        self.temperature = temp
 
 
 class Gas(Status):
     status = "g"
 
-    def __init__(self, temperature=super().temperature):
-        self.temperature = temperature
-
 
 class Liquid(Status):
     status = "l"
-
-    def __init__(self, temperature=super().temperature):
-        self.temperature = temperature
 
 
 class Solid(Status):
     status = "s"
 
-    def __init__(self, temperature=super().temperature):
-        self.temperature = temperature
-
 
 class Aqua(Status):
     status = "aq"
-
-    def __init__(self, temperature=super().temperature):
-        self.temperature = temperature
