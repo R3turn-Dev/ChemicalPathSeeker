0ba5cf64cd5a34d91b0ec4ea53a6258128049141
Eunhak Lee <admin@return0927.xyz>
2019-07-11 20:07:32 +0900

    Added main structure of Parser

--- parser.py
+++ parser.py
@@ -2,6 +2,30 @@
 # Parser
 # Author: Eunhak Lee(@return0927)
 #
-
 from Chem.status import *
 from Chem.prototype import Atom, Compound
+from traceback import format_exc
+
+
+class Parser:
+    def __init__(self):
+        self.algorithm = []
+        self.status = 0
+
+    def run(self, data):
+        self.status = 1
+
+        try:
+            for func in self.algorithm:
+                data = func(data)
+
+            self.status = 0
+        except:
+            self.status = -1
+
+            data = format_exc()
+
+        finally:
+            return data
+
+
