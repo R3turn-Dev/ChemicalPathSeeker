8dfec6fa73340d5909cc2451678c7962261e74cd
Eunhak Lee <admin@return0927.xyz>
2019-07-11 19:36:41 +0900

    Added atomic variables

--- Chem/prototype.py
+++ Chem/prototype.py
@@ -18,6 +18,8 @@ class Atom(Element):
     def __init__(self, no):
         super().__init__(no)
 
+        self.electrons = self.atomic_number
+
     @property
     def atomic_repr(self):
         return repr(TABLE[self.atomic_number]())
@@ -25,6 +27,14 @@ class Atom(Element):
     @property
     def atomic_name(self):
         return TABLE[self.atomic_number].atomic_name
+
+    @property
+    def proton(self):
+        return self.atomic_number
+
+    @staticmethod
+    def derive_from_atomic_number(num):
+        return Atom(num)
 
 
 class Compound:
