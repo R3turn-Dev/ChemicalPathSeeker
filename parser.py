b43f3de7b17782f905a8730b4545081cb0bd85bf
EunhakLee <admin@return0927.xyz>
2019-07-12 03:45:40 +0900

    Adding parser functions
    - an Equation > reactants & products > Compound
    - detaching status quo of Compound

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@
 # Author: Eunhak Lee(@return0927)
 #
 from Chem.status import *
-from Chem.prototype import Atom, Compound
+from Chem.prototype import Atom, Compound, Equation
 from traceback import format_exc
 
 
@@ -28,4 +28,39 @@ class Parser:
         finally:
             return data
 
+    @staticmethod
+    def separate_compounds(data):
+        """
+
+        :param data: string of compounds or an equation
+        :return: Compound or Equation
+        """
+        # Equation mode
+        if "->" in data:
+            before, after = data.split("->")
+            before = [x.strip() for x in before.split("+")]
+            after = [x.strip() for x in after.split("+")]
+
+        # Compounds mode
+        else:
+            compounds = [x.split() for x in data.split("+")]
+
+    @staticmethod
+    def parse_compound(data):
+        """Convert compound string into compound class
+
+        :param data: string of a compound
+        :return: class Chem.prototype.Compound
+        """
+        count = 1
+
+        # Starting with count of compound
+        if data[0].isnumeric():
+            count = int(data[0])
+            data = data[1:]
+
+        # If indicating status or sth else
+        if ("(" in data and ")" in data) and \
+                0 <= data.index("(") < data.index(")") < len(data):
+            compound, status = data[:data.index("(")], data[data.index("("):]
 
