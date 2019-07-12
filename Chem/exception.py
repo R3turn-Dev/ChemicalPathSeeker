1eb11632c4eafda5b06349dc4a45dc2caaaf572c
EunhakLee <admin@return0927.xyz>
2019-07-12 09:38:49 +0900

    Added Chem exceptions

---
+++ Chem/exception.py
@@ -0,0 +1,9 @@
+class ChemException:
+    def __init__(self, msg: str = ""):
+        self.message = msg
+
+
+class ParseError(ChemException):
+    def __init__(self, msg: str = "", data: str = ""):
+        super().__init__(msg)
+        self.data = data
