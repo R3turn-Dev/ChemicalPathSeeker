6203096bc2d02eaa2524f5415fc24dea2bc73e93
EunhakLee <admin@return0927.xyz>
2019-07-12 10:58:45 +0900

    Updated case-tester

--- tester.py
+++ tester.py
@@ -1,9 +1,22 @@
 import logging
+import traceback
+from Chem.parser import Parser
+from Chem.exception import ParseError
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 # Read cases from case.txt
-cases = open("Case.txt").readlines()
+cases = open("Case.txt", encoding="UTF-8").read().split("\n")
 logging.info(f"Read {len(cases)} cases.")
 
+for case in cases:
+    parser = Parser()
+    try:
+        print(f" > {case }")
+        data = parser.parse_compound(case)
+        print(f" < {data}")
+    except ParseError as ex:
+        print(ex.data)
+        traceback.print_exc()
+        raise ex
