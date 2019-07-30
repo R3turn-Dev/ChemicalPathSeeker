282ea33ebe9fd0b30aab4ca1b95d752f6403ab09
EunhakLee <admin@return0927.xyz>
2019-07-30 12:00:15 +0900

    Added __add__, __sub__ for Equation and ThermochemicalEquation
    and also restructed repr system of Compound

--- Chem/prototype.py
+++ Chem/prototype.py
@@ -6,11 +6,25 @@ from .status import Status
 from .atoms import Element
 from .periodictable import TABLE, NAME2NUM
 from typing import List
+from collections import OrderedDict
+
+
+class CountedDict(OrderedDict):
+    def __add__(self, other):
+        out = self.copy()
+
+        for k, v in other.items():
+            if k in out:
+                out[k].count += v.count
+            else:
+                out[k] = v
+
+        return out
 
 
 class Atom(Element):
     def __repr__(self):
-        return f"""class <Atom({self.atomic_repr}{self.atomic_number})>"""
+        return f"""class <Atom({self.atomic_repr}, {self.atomic_number})>"""
 
     def __str__(self):
         return self.atomic_repr
@@ -48,39 +62,59 @@ class Compound:
     def __str__(self):
         return repr(self)
 
+    def __add__(self, other):
+        atoms = self.atoms.copy()
+
+        for atom in other.atoms:
+            if atom.atomic_number not in atoms:
+                atoms[atom.atomic_number] = 0
+
+            atoms[atom.atom.atomic_number] += 1
+
+        return Compound.from_ordered_dict(atoms)
+
     @property
     def chemical_formula(self):
         out = str(self.count) if self.count > 1 else ""
-        heading = ""
-        count = 0
-        i = 0
 
-        while i < len(self.atoms):
-            if heading != self.atoms[i].atomic_repr:
-                if count > 1:
-                    out += str(count)
+        for k, v in self.atoms.items():
+            if not v:
+                continue
 
-                heading = self.atoms[i].atomic_repr
+            out += "{}{}".format(Atom.derive_from_atomic_number(k).atomic_repr, v if v > 1 else "")
 
-                out += heading
-                count = 1
-            else:
-                count += 1
+        return out + str(self.status)
 
-            i += 1
+    @staticmethod
+    def from_ordered_dict(atoms: OrderedDict, count: int = 1, status: Status = Status()):
+        _atoms = []
 
-        if count > 1:
-            out += str(count)
+        for k, v in atoms:
+            for n in range(v):
+                _atoms.append(Atom.derive_from_atomic_number(k))
 
-        return out
+        return Compound(_atoms, count, status)
+
+    @staticmethod
+    def from_chemical_formula(formula: str, count: int = 1, status: Status = Status()):
+        pass
 
     def __init__(self, atoms: List[Atom], count: int = 1, status: Status = Status()):
         self.count = count
-        self.atoms = [*atoms]
         self.status = status
+        self.atoms = OrderedDict()
+
+        for atom in atoms:
+            if atom.atomic_number not in self.atoms:
+                self.atoms[atom.atomic_number] = 0
+
+            self.atoms[atom.atomic_number] += 1
 
 
 class Equation:
+    reactants: List[Compound]
+    products: List[Compound]
+
     def __repr__(self):
         return f"""class {self.__class__.__name__}<{str(self)}>"""
 
@@ -90,9 +124,38 @@ class Equation:
             " + ".join([x.chemical_formula for x in self.products])
             )
 
+    def __add__(self, other):
+        reactants = CountedDict(
+            (x.chemical_formula[1:] if x.count > 1 else x.chemical_formula, x) for x in self.reactants
+            ) + CountedDict(
+            (x.chemical_formula[1:] if x.count > 1 else x.chemical_formula, x) for x in other.reactants
+            )
+
+        products = CountedDict(
+            (x.chemical_formula[1:] if x.count > 1 else x.chemical_formula, x) for x in self.products
+            ) + CountedDict(
+            (x.chemical_formula[1:] if x.count > 1 else x.chemical_formula, x) for x in other.products
+            )
+
+        for elem in {*reactants} & {*products}:
+            if reactants[elem].count > products[elem].count:
+                reactants[elem].count -= products[elem].count
+                del products[elem]
+            else:
+                products[elem].count -= reactants[elem].count
+                del reactants[elem]
+
+        return Equation([*reactants.values()], [*products.values()])
+
+    def __sub__(self, other):
+        return self + other.reverse()
+
     def __init__(self, reactants: List[Compound], products: List[Compound]):
         self.reactants = [*reactants]
         self.products = [*products]
+
+    def reverse(self):
+        return Equation(self.products, self.reactants)
 
 
 class ThermochemicalEquation(Equation):
@@ -104,6 +167,19 @@ class ThermochemicalEquation(Equation):
             "{:,}J".format(self.enthalpy)
             )
 
+    def __add__(self, other):
+        return ThermochemicalEquation.from_equation(
+            Equation.__add__(self, other),
+            enthalpy=self.enthalpy + other.enthalpy,
+            )
+
     def __init__(self, reactants: List[Compound], products: List[Compound], enthalpy: int = 0):
         super().__init__(reactants, products)
         self.enthalpy = enthalpy
+
+    @staticmethod
+    def from_equation(equation: Equation, enthalpy: int = 0):
+        return ThermochemicalEquation(equation.reactants, equation.products, enthalpy)
+
+    def reverse(self):
+        return ThermochemicalEquation(self.products, self.reactants, -self.enthalpy)
--- Chem/status.py
+++ Chem/status.py
@@ -7,6 +7,15 @@
 class Status:
     status = ""
 
+    def __str__(self):
+        data = ", ".join(x for x in [
+                self.status,
+                self.temperature if self.temperature != 273.0 else "",
+                self.meta
+                ] if x
+            )
+        return "{}".format("(" + data + ")" if data else "")
+
     def __init__(self, meta: str = "", temp: float = 273):
         self.temperature = temp
         self.meta = meta
